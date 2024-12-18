63a8f7c28f7b4e4b80ee189b7ec459df5678cb1f
oussamaamou <amoussdark@gmail.com>
2024-12-18 15:25:13 +0100

    Modifier le fichier JavaScript

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const ctnr1 = document.getElementById("ctnrcsltion");
+const xmark1 = document.getElementById("xmarkcsltion");
+
+xmark1.addEventListener('click', function(){
+    ctnr1.style.display = 'none';
+});
