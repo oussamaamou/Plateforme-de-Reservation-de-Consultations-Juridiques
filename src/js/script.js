0f8e8bcf63de71106c0d6e617d35293bdf338cb2
oussamaamou <amoussdark@gmail.com>
2024-12-18 23:32:40 +0100

    L'Ajout des fonctions en JavaScript

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,12 @@ const xmark1 = document.getElementById("xmarkcsltion");
 xmark1.addEventListener('click', function(){
     ctnr1.style.display = 'none';
 });
+
+
+const cfrmbttn1 = document.getElementById("cfrmrsrvtion");
+const annltblelmt = document.getElementById("annltbl");
+
+cfrmbttn1.addEventListener('click', function(){
+    annltblelmt.style.display = 'none';
+});
+
