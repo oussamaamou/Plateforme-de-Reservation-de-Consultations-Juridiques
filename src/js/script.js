afb1d0b56b9e50a0c5eda872971073cf370031bb
oussamaamou <amoussdark@gmail.com>
2024-12-20 19:15:05 +0100

    L'Ajout du fichier JavaScript

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,29 @@
 const ctnr1 = document.getElementById("ctnrcsltion");
 const xmark1 = document.getElementById("xmarkcsltion");
 
-xmark1.addEventListener('click', function(){
+ 
+xmark1?.addEventListener('click', function(){
     ctnr1.style.display = 'none';
 });
 
 
-const cfrmbttn1 = document.getElementById("cfrmrsrvtion");
-const annltblelmt = document.getElementById("annltbl");
 
-cfrmbttn1.addEventListener('click', function(){
-    annltblelmt.style.display = 'none';
+const cfrmbttn1 = document.getElementById("cfrmrsrvtion");
+const annltblelmt = document.querySelectorAll('.annltbl');
+
+cfrmbttn1?.addEventListener('click', function(){
+    annltblelmt.forEach(element => {
+        element.style.display = 'none';
+    })
 });
+
+/////////////////////////////////////////////////////////////////////////
+
+const dltebttn1 = document.getElementById("dltersrvtion");
+const cardrsrvtion = document.getElementById("rsrvtioncard");
+
+dltebttn1?.addEventListener('click', function(){
+    cardrsrvtion.style.display = 'none';
+});
+
 
