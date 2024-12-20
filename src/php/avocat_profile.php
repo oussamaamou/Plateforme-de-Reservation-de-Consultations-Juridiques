0f2c2a1f072f88a67f49372daf74981e42d1a255
oussamaamou <amoussdark@gmail.com>
2024-12-20 19:12:31 +0100

    Profile partie avocat

--- src/php/avocat_profile.php
+++ src/php/avocat_profile.php
@@ -50,6 +50,67 @@
     </header>
 
     <main>
+
+        <!-- Reservation Form-->
+        <div id="ctnrcsltion" class="fixed left-[32rem] top-[0rem] flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <i id="xmarkcsltion" class="fa-solid fa-xmark ml-[26rem] text-2xl cursor-pointer mt-[1.2rem]" style="color: #2e2e2e;"></i>
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="text-xl mt-[-2rem] font-bold leading-tight tracking-tight text-stone-700 md:text-2xl dark:text-white">
+                        Reserver une Consultation
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" action="#">
+                        <div class="grid lg:grid-cols-2 gap-6">
+                            <div>
+                                <label for="nom" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Nom</label>
+                                <input type="text" name="nom" id="nom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" >
+                            </div>
+                            <div>
+                                <label for="prenom" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Prenom</label>
+                                <input type="text" name="prenom" id="prenom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" >
+                            </div>
+                        </div>
+                        <div class="grid lg:grid-cols-2 gap-6">
+                            <div>
+                                <label for="telephone" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Téléphone</label>
+                                <input type="text" name="telephone" id="telephone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" >
+                            </div>
+                            <div>
+                                <label for="email" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Email</label>
+                                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" >
+                            </div>
+                        </div>
+                        <div class="grid lg:grid-cols-2 gap-6">
+                            <div>
+                                <label for="password" class="block mb-2 text-sm font-medium text-stone-700 dark:text-white">Password</label>
+                                <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" >
+                            </div>
+                            <div>
+                                <label class="block mb-2 text-sm font-medium text-stone-700 dark:text-white" for="photo">Ajouter une photo</label>
+                                <input name="photo" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-[7px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="file">
+                            </div>
+                        </div>
+                        <div>
+                            <label for="specialite" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Spécialité </label>
+                            <select id="specialite" name="specialite" class="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option value="Droit bancaire et boursier">Droit bancaire et boursier</option>
+                                <option value="Droit commercial, des affaires et de la concurrence">Droit commercial, des affaires et de la concurrence</option>
+                                <option value="Droit des garanties, des sûretés et des mesures d'exécution">Droit des garanties, des sûretés et des mesures d'exécution</option>
+                                <option value="Droit de la sécurité sociale et de la protection sociale">Droit de la sécurité sociale et de la protection sociale</option>
+                                <option value="Droit de la protection des données personnelles">Droit de la protection des données personnelles</option>
+                                <option value="Droit du numérique et des communications">Droit du numérique et des communications</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+                            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Écrivez votre biographie ici..."></textarea>
+                        </div>
+                        <button type="submit" class="ml-[7rem] w-[8rem] text-white bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Confirmer</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
         <section>
             <div>
                 <div class="container mx-auto py-8">
@@ -143,5 +204,7 @@
         </div>
     </footer>
     
+
+    <script src="../js/script.js"></script>
 </body>
 </html>
