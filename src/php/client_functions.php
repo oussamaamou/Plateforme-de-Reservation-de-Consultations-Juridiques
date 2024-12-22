a07230d8a7a5fb5d88db922b421b9ddf4b65fb12
oussamaamou <amoussdark@gmail.com>
2024-12-22 23:48:11 +0100

    Regler at ajouter des nouvelles fonctions

--- src/php/client_functions.php
+++ src/php/client_functions.php
@@ -61,10 +61,66 @@ function getClientProfile($ID) {
     }
 }
 
+// Afficher les Consultations des Avocats
 function getAllConsultations(){
     global $conn;
     $result = $conn->query("SELECT * FROM utilisateur WHERE Role = 'Avocat' ");
     return $result->fetch_all(MYSQLI_ASSOC);
+}
+
+// Reserver une Consultation
+function addReservation($ID_Client, $ID_Avocat, $Date_Consultation, $Statut) {
+    global $conn;
+    $stmt = $conn->prepare("INSERT INTO reservation (ID_Client, ID_Avocat, Date_Consultation, Statut) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("iiss",$ID_Client, $ID_Avocat, $Date_Consultation, $Statut);
+    return $stmt->execute();
+}
+
+// Afficher les Reservations
+function getAllReservation($client_id){
+    global $conn;
+
+    $sql = "
+        SELECT 
+            r.ID AS reservation_id, 
+            r.Date_Consultation, 
+            r.Statut, 
+            c.Nom AS client_nom, 
+            c.Prenom AS client_prenom, 
+            c.Email AS client_email, 
+            c.Photo AS client_photo,
+            a.Nom AS avocat_nom, 
+            a.Prenom AS avocat_prenom,
+            a.Email AS avocat_email,
+            a.Photo AS avocat_photo
+        FROM 
+            reservation r
+        JOIN 
+            utilisateur c ON r.ID_Client = c.ID
+        JOIN 
+            utilisateur a ON r.ID_Avocat = a.ID
+        WHERE 
+            r.ID_Client = ?";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $client_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    return $result->fetch_all(MYSQLI_ASSOC);
+
+    $conn->close();
+}
+
+
+// Annuler une Reservation
+function annulerReservation($id) {
+    global $conn;
+
+    $stmt = $conn->prepare("DELETE FROM reservation WHERE id = ?");
+    $stmt->bind_param("i", $id);
+
+    return $stmt->execute();
 }
 
 ?>
