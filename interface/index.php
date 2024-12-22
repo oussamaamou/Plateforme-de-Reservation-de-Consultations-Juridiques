df8d5067815a07970d5df771ec47cfccbba70f67
oussamaamou <amoussdark@gmail.com>
2024-12-22 15:10:08 +0100

    Modification de l'interface visiteur

--- interface/index.php
+++ interface/index.php
@@ -14,7 +14,7 @@
     
     <title>Accueil</title>
 </head>
-<body>
+<body class="bg-stone-300">
 
     <header>
         <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
@@ -31,27 +31,11 @@
                         <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </button>
                 </div>
-                <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1 mt-[-3rem]" id="mobile-menu-2">
-                    <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                        <li>
-                            <a href="index.php" class="block py-2 pr-4 pl-3 text-stone-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Home</a>
-                        </li>
-                        <li>
-                            <a href="../src/php/avocat_profile.php" class="block py-2 pr-4 pl-3 text-stone-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Profile</a>
-                        </li>
-                        <li>
-                            <a href="../src/php/afficher_reservations.php" class="block py-2 pr-4 pl-3 text-stone-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Reservations</a>
-                        </li>
-                        <li>
-                            <a href="../src/php/afficher_consultations.php" class="block py-2 pr-4 pl-3 text-stone-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Consultations</a>
-                        </li>
-                    </ul>
-                </div>
             </div>
         </nav>
     </header>
 
-    <main>
+    <main class="pt-[5rem]">
 
         <section class="flex gap-x-[6rem]">
             <div class="ml-[13rem] mt-[4rem]">
@@ -103,8 +87,10 @@
                     </div>
 
                     <div class="max-md:text-center">
-                    <p class="w-[35rem] text-white text-center text-lg font-semibold mt-4 leading-relaxed">Notre mission est d'aider les gens à accéder facilement à une assistance juridique de qualité. Nous comprenons que les démarches juridiques peuvent être compliquées et stressantes. C'est pourquoi notre site web est conçu pour rendre la consultation d'un avocat simple, rapide et accessible à tous. Que vous ayez une question urgente ou que vous ayez besoin de conseils à long terme, nous sommes là pour vous accompagner tout au long de votre parcours juridique.</p>
-                    <button type="button" class="ml-[15rem] px-5 py-2.5 mt-8 bg-yellow-500 hover:opacity-80 text-white font-semibold tracking-wider rounded text-sm outline-none">Explorer</button>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-8 fill-white inline rotate-180" viewBox="0 0 475.082 475.081">
+                        <path d="M164.454 36.547H54.818c-15.229 0-28.171 5.33-38.832 15.987C5.33 63.193 0 76.135 0 91.365v109.632c0 15.229 5.327 28.169 15.986 38.826 10.66 10.656 23.606 15.988 38.832 15.988h63.953c7.611 0 14.084 2.666 19.414 7.994 5.33 5.325 7.994 11.8 7.994 19.417v9.131c0 20.177-7.139 37.397-21.413 51.675-14.275 14.271-31.499 21.409-51.678 21.409h-18.27c-4.952 0-9.233 1.813-12.851 5.427-3.615 3.614-5.424 7.898-5.424 12.847v36.549c0 4.941 1.809 9.233 5.424 12.848 3.621 3.613 7.898 5.427 12.851 5.427h18.271c19.797 0 38.688-3.86 56.676-11.566 17.987-7.707 33.546-18.131 46.68-31.265 13.131-13.135 23.553-28.691 31.261-46.679 7.707-17.987 11.562-36.877 11.562-56.671V91.361c0-15.23-5.33-28.171-15.987-38.828s-23.602-15.986-38.827-15.986zm294.635 15.987c-10.656-10.657-23.599-15.987-38.828-15.987H310.629c-15.229 0-28.171 5.33-38.828 15.987-10.656 10.66-15.984 23.601-15.984 38.831v109.632c0 15.229 5.328 28.169 15.984 38.826 10.657 10.656 23.6 15.988 38.828 15.988h63.953c7.611 0 14.089 2.666 19.418 7.994 5.324 5.328 7.994 11.8 7.994 19.417v9.131c0 20.177-7.139 37.397-21.416 51.675-14.274 14.271-31.494 21.409-51.675 21.409h-18.274c-4.948 0-9.233 1.813-12.847 5.427-3.617 3.614-5.428 7.898-5.428 12.847v36.549c0 4.941 1.811 9.233 5.428 12.848 3.613 3.613 7.898 5.427 12.847 5.427h18.274c19.794 0 38.684-3.86 56.674-11.566 17.984-7.707 33.541-18.131 46.676-31.265 13.134-13.135 23.562-28.695 31.265-46.679 7.706-17.983 11.563-36.877 11.563-56.671V91.361c-.003-15.23-5.328-28.171-15.992-38.827z" data-original="#000000"></path>
+                    </svg>
+                    <p class="w-[35rem] text-white text-center text-xl font-bold mt-4">Notre mission est d'aider les gens à accéder facilement à une assistance juridique de qualité. Nous comprenons que les démarches juridiques peuvent être compliquées et stressantes. C'est pourquoi notre site web est conçu pour rendre la consultation d'un avocat simple, rapide et accessible à tous. Que vous ayez une question urgente ou que vous ayez besoin de conseils à long terme, nous sommes là pour vous accompagner tout au long de votre parcours juridique.</p>
                     </div>
                 </div>
             </div>
